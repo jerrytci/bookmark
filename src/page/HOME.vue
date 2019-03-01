087c8f6a72208c592f0c554b75f145c07920a212
jerry <code.ci@qq.com>
2019-03-01 11:04:13 +0800

    add list method

--- src/page/HOME.vue
+++ src/page/HOME.vue
@@ -19,17 +19,18 @@
         <el-card class="box-card" :body-style="{padding: '0px'}" :style="{margin: _px(itemMeta.margin)}">
           <div class="label" style="width: inherit">
             <div class="label-title">
-              <a>{{list.title === '' ? '未设置名字' : list.title}}</a>
+              <a @click="restoreList(index, true)">{{list.title === '' ? '未设置名字' : list.title}}</a>
             </div>
-            <i class="el-icon-edit-outline"></i>
-            <i class="el-icon-remove-outline"></i>
+            <i class="el-icon-star-off"></i>
+            <i class="el-icon-edit-outline" @click="renameList(index, list.title)"></i>
+            <i class="el-icon-remove-outline" @click="removeList(index)"></i>
           </div>
-          <div class="link" style="width: inherit" v-for="(tab) in list.tabs">
+          <div class="link" style="width: inherit" v-for="(tab, tabIndex) in list.tabs">
             <div class="link-title">
               <img :src="tab.favIconUrl == null ? '/assets/icons/icon_16.png' : tab.favIconUrl">
               <a :href="tab.url" target="_blank">{{tab.title}}</a>
             </div>
-            <i class="el-icon-remove-outline"></i>
+            <i class="el-icon-remove-outline" @click="tabRemove(index, tabIndex)"></i>
           </div>
         </el-card>
       </waterfall-slot>
@@ -38,10 +39,12 @@
 </template>
 
 <script>
-  import $ from 'jquery'
+  // import $ from 'jquery'
+  import _ from 'lodash'
   import Waterfall from 'vue-waterfall/lib/waterfall'
   import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
   import storage from "@/common/storage";
+  import tabs from "@/common/tabs";
 
   export default {
     name: "Home",
@@ -78,7 +81,6 @@
       }
     },
     created() {
-      let _this = this;
       // 获取unclassified links
       storage.getLists().then(lists => {
         if (lists) {
@@ -103,6 +105,50 @@
       itemHeight: function (linkCount) {
         return linkCount * this.itemMeta.content + this.itemMeta.header + this.itemMeta.margin * 2;
       },
+
+      /*tab*/
+      tabRemove(listIndex, tabIndex) {
+        this.lists[listIndex].tabs.splice(tabIndex, 1);
+        if (this.lists[listIndex].tabs.length === 0)
+          this.removeList(listIndex);
+        this.storeLists()
+      },
+      storeLists: _.debounce(function () {
+        console.time('store');
+        this.$nextTick(() => {
+          storage.setLists(this.lists).then(() => console.timeEnd('store'))
+        })
+      }, 200),
+      restoreList(listIndex, inNewWindow = false) {
+        if (inNewWindow) tabs.restoreListInNewWindow(this.lists[listIndex]);
+        else tabs.restoreList(this.lists[listIndex]);
+        if (this.lists[listIndex].pinned) return;
+        this.removeList(listIndex)
+      },
+      //remove list
+      removeList(listIndex) {
+        this.lists.splice(listIndex, 1);
+        this.storeLists()
+      },
+      renameList(listIndex, name) {
+        const _this = this;
+        this.$prompt('请输入名字', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          inputPattern: /[^\s]/,
+          inputValue: name,
+          inputErrorMessage: '名字格式不正确'
+        }).then(({value}) => {
+          if (name === value.trim()) {
+            return;
+          }
+          _this.lists[listIndex].title = value.trim();
+          _this.storeLists();
+          this.$message({type: 'success', message: '更改成功'});
+        }).catch(() => {
+          this.$message({type: 'info', message: '已取消更改'});
+        });
+      },
     },
     mounted() {
     }
@@ -114,7 +160,7 @@
 </style>
 <style lang="stylus" scoped>
   .label-title
-    width calc(100% - 24px * 2)
+    width calc(100% - 24px * 3)
 
   /*10:padding.*/
   .link-title
