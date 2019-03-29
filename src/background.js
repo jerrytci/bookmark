c0c2d1ecba8c2cb285a57e3b4897efe813982e4f
jerry <code.ci@qq.com>
2019-03-30 00:21:15 +0800

    [indent code] indent background, add 分号

--- src/background.js
+++ src/background.js
@@ -8,42 +8,42 @@ import browser from 'webextension-polyfill'
 if (DEBUG) import(
   /* webpackChunkName: "autoreload", webpackMode: "lazy" */
   './common/util/autoreload'
-  ).then(({autoreload}) => autoreload())
+  ).then(({autoreload}) => autoreload());
 
 if (PRODUCTION) import(
   /* webpackChunkName: "tracker", webpackMode: "lazy" */
   '@/common/util/tracker'
-  ).then(({tracker}) => tracker())
+  ).then(({tracker}) => tracker());
 
 if (DEBUG) {
-  window.tabs = tabs
+  window.tabs = tabs;
   window.browser = browser
 }
 
 const getBrowserActionHandler = action => {
-  if (action === 'store-selected') return () => tabs.storeSelectedTabs()
-  if (action === 'show-list') return () => tabs.openTabLists()
-  if (action === 'store-all') return () => tabs.storeAllTabs()
-  if (action === 'store-all-in-all-windows') return () => tabs.storeAllTabInAllWindows()
+  if (action === 'store-selected') return () => tabs.storeSelectedTabs();
+  if (action === 'show-list') return () => tabs.openTabLists();
+  if (action === 'store-all') return () => tabs.storeAllTabs();
+  if (action === 'store-all-in-all-windows') return () => tabs.storeAllTabInAllWindows();
   return () => {
   }
-}
+};
 
 const updateBrowserAction = (action, tmp = false) => {
-  if (!tmp) window.currentBrowserAction = action
-  const items = _.find(options.optionsList, {name: 'browserAction'}).items
-  const label = _.find(items, {value: action}).label
-  console.log('action is: ', action, 'set title as: ', label)
-  browser.browserAction.setTitle({title: label})
+  if (!tmp) window.currentBrowserAction = action;
+  const items = _.find(options.optionsList, {name: 'browserAction'}).items;
+  const label = _.find(items, {value: action}).label;
+  console.log('action is: ', action, 'set title as: ', label);
+  browser.browserAction.setTitle({title: label});
   if (action === 'popup') {
-    browser.browserAction.setPopup({popup: 'index.html#/popup'})
+    browser.browserAction.setPopup({popup: 'index.html#/popup'});
     window.coverBrowserAction = () => {
     }
   } else {
-    browser.browserAction.setPopup({popup: ''})
-    window.browswerActionClickedHandler = getBrowserActionHandler(action)
+    browser.browserAction.setPopup({popup: ''});
+    window.browswerActionClickedHandler = getBrowserActionHandler(action);
     if (window.opts.openTabListWhenNewTab) window.coverBrowserAction = async activeInfo => {
-      const tab = await browser.tabs.get(activeInfo.tabId)
+      const tab = await browser.tabs.get(activeInfo.tabId);
       if (['about:home', 'chrome://newtab/'].includes(tab.url)) {
         updateBrowserAction('show-list', true)
       } else {
@@ -51,12 +51,12 @@ const updateBrowserAction = (action, tmp = false) => {
       }
     }
   }
-}
+};
 
 const setupContextMenus = async pageContext => {
-  await browser.contextMenus.removeAll()
-  const contexts = [browser.contextMenus.ContextType.BROWSER_ACTION]
-  if (pageContext) contexts.push(browser.contextMenus.ContextType.PAGE)
+  await browser.contextMenus.removeAll();
+  const contexts = [browser.contextMenus.ContextType.BROWSER_ACTION];
+  if (pageContext) contexts.push(browser.contextMenus.ContextType.PAGE);
   const menus = {
     SHOW_TAB_LIST: tabs.openTabLists,
     STORE_SELECTED_TABS: tabs.storeSelectedTabs,
@@ -65,105 +65,105 @@ const setupContextMenus = async pageContext => {
     STORE_TWOSIDE_TABS: tabs.storeTwoSideTabs,
     STORE_ALL_TABS_IN_CURRENT_WINDOW: tabs.storeAllTabs,
     STORE_ALL_TABS_IN_ALL_WINDOWS: tabs.storeAllTabInAllWindows,
-  }
+  };
   const createMenus = async (obj, parent) => {
     for (const key of Object.keys(obj)) {
       const prop = {
         id: key,
         title: __('menu_' + key),
         contexts,
-      }
+      };
       if (parent) {
-        prop.id = parent + '.' + key
+        prop.id = parent + '.' + key;
         prop.parentId = parent
       }
-      const id = await browser.contextMenus.create(prop)
-      console.log('context menu created: ' + id)
+      const id = await browser.contextMenus.create(prop);
+      console.log('context menu created: ' + id);
       if (_.isObject(obj[key])) await createMenus(obj[key], key)
     }
-  }
-  createMenus(menus)
+  };
+  createMenus(menus);
   window.contextMenusClickedHandler = info => _.get(menus, info.menuItemId)()
-}
+};
 
 const dynamicDisableMenu = async () => {
-  const groupedTabs = await tabs.groupTabsInCurrentWindow()
-  console.log(groupedTabs)
+  const groupedTabs = await tabs.groupTabsInCurrentWindow();
+  console.log(groupedTabs);
   browser.contextMenus.update('EXTRA.STORE_LEFT_TABS', {
     enabled: groupedTabs.left.length !== 0,
     title: __('menu_STORE_LEFT_TABS') + ` (${groupedTabs.left.length})`,
-  })
+  });
   browser.contextMenus.update('EXTRA.STORE_RIGHT_TABS', {
     enabled: groupedTabs.right.length !== 0,
     title: __('menu_STORE_RIGHT_TABS') + ` (${groupedTabs.right.length})`,
-  })
+  });
   browser.contextMenus.update('EXTRA.STORE_TWOSIDE_TABS', {
     enabled: groupedTabs.twoSide.length !== 0,
     title: __('menu_STORE_TWOSIDE_TABS') + ` (${groupedTabs.twoSide.length})`,
   })
-}
+};
 
 const init = async () => {
-  const opts = window.opts = await storage.getOptions() || {}
-  _.defaults(opts, options.getDefaultOptions())
-  await storage.setOptions(opts)
-  updateBrowserAction(opts.browserAction)
-  setupContextMenus(opts.pageContext)
+  const opts = window.opts = await storage.getOptions() || {};
+  _.defaults(opts, options.getDefaultOptions());
+  await storage.setOptions(opts);
+  updateBrowserAction(opts.browserAction);
+  setupContextMenus(opts.pageContext);
   browser.runtime.onMessage.addListener(async msg => {
-    console.log(msg)
+    console.log(msg);
     if (msg.optionsChanged) {
-      const changes = msg.optionsChanged
-      console.log(changes)
-      Object.assign(opts, changes)
-      if (changes.browserAction) updateBrowserAction(changes.browserAction)
-      if ('pageContext' in changes) await setupContextMenus(changes.pageContext)
-      await browser.runtime.sendMessage({optionsChangeHandledStatus: 'success'})
+      const changes = msg.optionsChanged;
+      console.log(changes);
+      Object.assign(opts, changes);
+      if (changes.browserAction) updateBrowserAction(changes.browserAction);
+      if ('pageContext' in changes) await setupContextMenus(changes.pageContext);
+      await browser.runtime.sendMessage({optionsChangeHandledStatus: 'success'});
       if (PRODUCTION) Object.keys(changes).map(key => ga('send', 'event', 'Options', key + ':' + changes[key]))
     }
     if (msg.restoreList) {
-      const restoreList = msg.restoreList
-      const listIndex = restoreList.index
-      const lists = await storage.getLists()
+      const restoreList = msg.restoreList;
+      const listIndex = restoreList.index;
+      const lists = await storage.getLists();
       if (restoreList.newWindow) {
         tabs.restoreListInNewWindow(lists[listIndex])
       } else {
         tabs.restoreList(lists[listIndex])
       }
       if (!lists[listIndex].pinned) {
-        lists.splice(listIndex, 1)
+        lists.splice(listIndex, 1);
         storage.setLists(lists)
       }
     }
-  })
+  });
   browser.runtime.onUpdateAvailable.addListener(detail => {
     window.update = detail.version
-  })
+  });
   browser.runtime.onInstalled.addListener(detail => {
     if (detail.reason === chrome.runtime.OnInstalledReason.UPDATE) {
       tabs.openTheFirstSidePage()
     }
-  })
+  });
   browser.browserAction.onClicked.addListener(action => window.browswerActionClickedHandler(action))
   browser.contextMenus.onClicked.addListener(info => window.contextMenusClickedHandler(info))
   browser.tabs.onActivated.addListener(_.debounce(activeInfo => {
-    window.coverBrowserAction(activeInfo)
+    window.coverBrowserAction(activeInfo);
     dynamicDisableMenu(activeInfo)
-  }, 200))
+  }, 200));
   browser.commands.onCommand.addListener(async command => {
-    if (command === 'store-selected-tabs') tabs.storeSelectedTabs()
-    else if (command === 'store-all-tabs') tabs.storeAllTabs()
+    if (command === 'store-selected-tabs') tabs.storeSelectedTabs();
+    else if (command === 'store-all-tabs') tabs.storeAllTabs();
     else if (command === 'restore-lastest-list') {
-      const lists = await storage.getLists()
-      if (lists.length === 0) return true
-      const lastest = lists[0]
-      await tabs.restoreList(lastest)
-      if (lastest.pinned) return true
-      lists.shift()
+      const lists = await storage.getLists();
+      if (lists.length === 0) return true;
+      const lastest = lists[0];
+      await tabs.restoreList(lastest);
+      if (lastest.pinned) return true;
+      lists.shift();
       return storage.setLists(lists)
-    } else if (command === 'open-lists') tabs.openTabLists()
-    else return true
+    } else if (command === 'open-lists') tabs.openTabLists();
+    else return true;
     if (PRODUCTION) ga('send', 'event', 'Command', 'used', command)
   })
-}
+};
 
-init()
+init();
